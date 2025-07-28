655e130cf943a1cc1865a6d21a149f1df089c40d
sathish <satish.kumar@obopay.com>
2025-07-28 16:18:18 +0530

    Added files

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+## 0.0.1
+
+Created a plugin to get user mobile number. Supports Android only.
+
+## 0.0.1+1
+
+Change in Readme.md File
+
+## 0.0.1+2
+
+Image Added in Readme.md File
+
+## 0.0.1+3
+
+Phone Number Reducer Added to seperate country code and mobile number
+
+## 0.0.1+4
+
+dispose() is not static function anymore , only accessible through class object.
+
+## 0.0.1+5
+
+Updated to null-safety
---
+++ example/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_number_picker/mobile_number_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  MobileNumberPicker mobileNumber = MobileNumberPicker();
+  MobileNumber mobileNumberObject = MobileNumber();
+
+  @override
+  void dispose() {
+    mobileNumber.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((timeStamp) => mobileNumber.mobileNumber());
+    mobileNumber.getMobileNumberStream.listen((MobileNumber? event) {
+      if (event != null &&
+          event.states == PhoneNumberStates.PhoneNumberSelected) {
+        setState(() {
+          mobileNumberObject = event;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mobile Number Plugin'),
+        ),
+        body: Center(
+          child: Text(
+              'Mobile Number: ${mobileNumberObject?.phoneNumber}\n Country Code: ${mobileNumberObject?.countryCode}'),
+        ),
+      ),
+    );
+  }
+}
